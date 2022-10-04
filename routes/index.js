7192b6c3ff6a22d9a20bc766ab2d10f254047f7f
mariavysotskaya <vysotskaya92@gmail.com>
2022-10-04 21:27:17 +0300

    non-existent route handle added

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const NotFoundError = require('../errors/not-found-err');
 
 router.use(require('./auth.routes'));
 
@@ -6,5 +7,9 @@ router.use(require('../middlewares/auth'));
 
 router.use(require('./users'));
 router.use(require('./movies'));
+
+router.use('*', () => {
+  throw new NotFoundError('Страница не найдена');
+});
 
 module.exports = router;
